c6ca389aaef4df1f55d1b927d1032f78dd9413af
HenriqueFT <31902340+HenriqueFT@users.noreply.github.com>
2017-09-25 14:32:47 -0300

    Adicao de testes
    -Feito .bat que roda deve estar junto dos demais arquivos,para que compile o programa e depois rode os testes ditos logo ao lado.
    -Pause inserido entre cada teste para que se podssa olhar antes.

---
+++ tests.bat
@@ -0,0 +1,14 @@
+set mypath=%~dp0
+gcc main.c -o b.exe
+b s
+pause
+b shp
+pause
+b sssssssssssssssssssssss
+pause
+b shhhsppphssshpphs
+pause
+b ppphhhhpppppsshhhspppsshhhhhhhhhhhsssssppp
+pause
+b shpshpshpshpshpshpshpshpshpshpshpshpshpshpshpshpshpshpshp
+pause
